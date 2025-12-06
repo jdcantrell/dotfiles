41c96aef53a9e13e9f02ebaf948c9b731d44019e
JD Cantrell <jd.cantrell@customer.io>
2025-12-05 20:29:12 -0800

    lots of updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,3 +9,21 @@ require("lazy").setup("plugins", {
     }
   })
 
+require "user.harpoon"
+
+
+-- at some point figure out a good home for this, glint isn't in lspconfig atm
+vim.lsp.config["glint"] = {
+  cmd = {'glint-language-server'},
+  filetypes= { 'html.handlebars', 'handlebars', 'typescript.glimmer', 'javascript.glimmer' },
+  root_markers = {
+    '.glintrc.yml',
+    '.glintrc',
+    '.glintrc.json',
+    '.glintrc.js',
+    'glint.config.js',
+    'package.json'
+  },
+};
+vim.lsp.enable('glint');
+
